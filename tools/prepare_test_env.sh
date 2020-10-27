9b3031ebfa4ecfb7b709424f48c0fe7098940265
Artem Goncharov <Artem.goncharov@gmail.com>
2020-10-27 16:01:40 +0100

    yet another try

--- tools/prepare_test_env.sh
+++ tools/prepare_test_env.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
 # TODO: maybe install collection to temp place
-ansible-galaxy collection install community.general 
+#ansible-galaxy collection install community.general 
 ansible-playbook playbooks/prepare_test_env.yaml -e ansible_python_interpreter=`which python`
 
-RANCHER_BIND_HOST=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
-
-export RANCHER_BIND_HOST
